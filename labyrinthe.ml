592a60869604fb87652cb2112b54cde760c7a689
takitsu21 <dylannbatisse@gmail.com>
2020-11-23 12:54:16 -0800

    path finding

--- labyrinthe.ml
+++ labyrinthe.ml
@@ -4,6 +4,8 @@ open Graphics;;
 let pacman_idx = ref 0;;
 let fantome_idx = ref 0;;
 let center = ref (0, 0);;
+let current_case_path_finding = ref 0;;
+let checkpoint = ref 0;;
 
 module type UFsig =
 sig
@@ -139,7 +141,58 @@ let verify_edges l h x =
 let make_sound () =
   sound 15000 500;;
 
-let cases_voisines l h =
+let update_voisines l h mur_present tmp_path q =
+  let xpath = !current_case_path_finding / l in
+  let ypath = !current_case_path_finding mod l in
+  let acc = ref 0 in
+  if verify_edges l h (!current_case_path_finding + l) && not tmp_path.(!current_case_path_finding + l) && not mur_present.(1).(xpath).(ypath)
+  then acc := !acc + 1;
+  if verify_edges l h (!current_case_path_finding - 1) && not tmp_path.(!current_case_path_finding - 1) && ypath >= 1 && not mur_present.(0).(xpath).(ypath - 1)
+  then acc := + 1;
+  if verify_edges l h (!current_case_path_finding + l) && not tmp_path.(!current_case_path_finding + l) && not mur_present.(1).(xpath).(ypath)
+  then acc := !acc + 1;
+  if verify_edges l h (!current_case_path_finding + 1) && not tmp_path.(!current_case_path_finding + 1) && !current_case_path_finding < ((l * h) - 1) && not mur_present.(0).(xpath).(ypath)
+  then acc := !acc + 1;
+  if !acc = 0 then current_case_path_finding := Queue.take q;
+  if !acc > 1 then checkpoint := !current_case_path_finding; Queue.add !checkpoint q;
+  (* if not tmp_path.(!current_case_path_finding) *)
+  (* then begin *)
+  if verify_edges l h (!current_case_path_finding - l) && not tmp_path.(!current_case_path_finding - l) && !current_case_path_finding >= l && not mur_present.(1).(xpath - 1).(ypath)
+  then begin current_case_path_finding := !current_case_path_finding - l; end
+  else if verify_edges l h (!current_case_path_finding - 1) && not tmp_path.(!current_case_path_finding - 1) && ypath >= 1 && not mur_present.(0).(xpath).(ypath - 1)
+  then begin current_case_path_finding := !current_case_path_finding - 1; end
+  else if verify_edges l h (!current_case_path_finding + l) && not tmp_path.(!current_case_path_finding + l) && not mur_present.(1).(xpath).(ypath)
+  then begin current_case_path_finding := !current_case_path_finding + l; end
+  else if verify_edges l h (!current_case_path_finding + 1) && not tmp_path.(!current_case_path_finding + 1) && !current_case_path_finding < ((l * h) - 1) && not mur_present.(0).(xpath).(ypath)
+  then current_case_path_finding := !current_case_path_finding + 1;
+  Queue.iter (fun i -> Printf.printf "%d\n" i) q;
+  Printf.printf "tmp_path %b\n" tmp_path.(!current_case_path_finding);;
+
+
+let draw_player x y c taille_case =
+  set_color c;
+  fill_circle (x) (y) (taille_case / 3);
+  set_color black;;
+
+let path_finding l h mur_present tmp_path pos_array q =
+  (* if !current_case_path_finding = !pacman_idx
+     then true
+     else begin
+     update_voisines l h mur_present;
+     Printf.printf "current_case_path_finding = %d pacman = %d\n" !current_case_path_finding !pacman_idx;
+     path_finding l h mur_present tmp_path;
+     end;; *)
+  (* tmp_path.(!current_case_path_finding) <- true; *)
+  while !pacman_idx <> !current_case_path_finding do
+
+    update_voisines l h mur_present tmp_path q;
+
+    tmp_path.(!current_case_path_finding) <- true;
+    draw_player (pos_array.(!current_case_path_finding).(0)) (pos_array.(!current_case_path_finding).(1)) green 25;
+
+    Printf.printf "path = %d pacman = %d checkpoint = %d\n" !current_case_path_finding !pacman_idx !checkpoint;
+  done;
+  tmp_path;;
 
 
 let move_pacman l h key mur_present =
@@ -162,10 +215,7 @@ let move_pacman l h key mur_present =
   | 'w' -> raise (invalid_arg "jeu fermer");
   | _ -> ();;
 
-let draw_player x y c taille_case =
-  set_color c;
-  fill_circle (x) (y) (taille_case / 3);
-  set_color black;;
+
 
 let gen_pacman_array_position upleftx uplefty l h taille_case =
   let pacman_pos = Array.init (l * h) (fun i -> [|i; i|]) in
@@ -192,13 +242,13 @@ let is_win l h =
   !pacman_idx = (l * h) - 1;;
 
 (* let rec est_relie src dst evite voisines =
-  if src = dst then true else begin
-  for i = 0 to (Array.length voisines.(dst)) - 1 do
+   if src = dst then true else begin
+   for i = 0 to (Array.length voisines.(dst)) - 1 do
     if (est_relie voisines.(i) dst src voisines)
     then true;
-  done;
-  false
-  end;; *)
+   done;
+   false
+   end;; *)
 
 let move_fantome l h =
   let pos = ref !fantome_idx in
@@ -230,6 +280,9 @@ let ia (upleftx, uplefty, l, h, pacman_pos, taille_case, mur_present) =
   while not (is_win l h) && (!pacman_idx <> !fantome_idx) do
     (* ignore (Unix.select [] [] [] 0.5); *)
     Unix.sleep 2;
+    let q = Queue.create () in
+    Queue.add !fantome_idx q;
+    let tmp_path = path_finding l h mur_present (Array.init (l * h) (fun i -> false)) pacman_pos q in
     if (not (is_win l h) && (!pacman_idx <> !fantome_idx)) (* double verification au cas ou pendant le Unix.sleep il y a un gagnant ou perdant *)
     then begin
       draw_player pacman_pos.(!fantome_idx).(0) pacman_pos.(!fantome_idx).(1) white taille_case; (* On redessine en blanc a la position d'avant *)
@@ -275,9 +328,9 @@ let draw_game upleftx uplefty l h taille_case =
   end;;
 
 let () =
-  let l = 10 in
-  let h = 10 in
-  let taille_case = ref 40 in
+  let l = 20 in
+  let h = 20 in
+  let taille_case = ref 25 in
   let upleftx = !taille_case / 2 in
   let uplefty = (h + 1) * !taille_case in
   let _ = Random.self_init () in
@@ -285,8 +338,11 @@ let () =
   let height = (h + 2) * !taille_case in
   let graph_size = " " ^ string_of_int width ^ "x" ^ string_of_int height in
   center := (width / 2, height / 2);
+
   open_graph graph_size;
   fantome_idx := (l - 1);
+  current_case_path_finding := !fantome_idx;
+  checkpoint := !fantome_idx;
   pacman_idx := 0;
   draw_game upleftx uplefty l h !taille_case;
   ignore @@ read_key ();;
